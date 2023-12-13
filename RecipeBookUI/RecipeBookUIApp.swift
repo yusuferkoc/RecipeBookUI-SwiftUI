b24ce8401fcbd3d986cc19b6011b43234485d859
yusuf <yusuferkoc99@gmail.com>
2023-12-13 14:04:00 +0300

    Initial Commit

---
+++ RecipeBookUI/RecipeBookUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeBookUIApp.swift
+//  RecipeBookUI
+//
+//  Created by Yusuf Erkoç on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeBookUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
